3483310d5729ee22bf8a579fe9cda974f91d12ec
Arslan Javaid <arslan.leo@live.com>
2018-01-04 22:22:34 +0500

    Adding jquery and bootstrap.

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 require('ol/ol.css');
+
+// Bootstrap wants jQuery global =(
+window.jQuery = $ = require('jquery');
+// Bootstrap doesn't have a "main" field / export anything =(
+var bootstrap = require('bootstrap/dist/js/bootstrap');
+// Get Bootstrap styles with cssify
+var style = require('./node_modules/bootstrap/dist/css/bootstrap.css');
+
 var ol_Map = require('ol/map').default;
 var ol_layer_Tile = require('ol/layer/tile').default;
 var ol_source_OSM = require('ol/source/osm').default;
